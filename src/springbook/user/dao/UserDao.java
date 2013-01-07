623984f4b87f22875a502db47d78e2030b06a7af
taerimmk <taerimmk@gmail.com>
2013-01-07 14:21:47 +0900

    20130107

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,8 +8,14 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
+	private ConnectionMaker connectionMaker;
+	
+	public UserDao(ConnectionMaker connectionMaker){
+		this.connectionMaker = connectionMaker;
+	}
+	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = getConnection();
+		Connection c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("insert into users(id, name, password) values (?,?,?)");
@@ -25,7 +30,7 @@ public class UserDao {
 	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = getConnection();
+		Connection c = connectionMaker.makeConnection();
 
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
@@ -47,33 +52,17 @@ public class UserDao {
 		return user;
 	}
 
-	private Connection getConnection() throws ClassNotFoundException,
-			SQLException {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection c = DriverManager.getConnection(
-				"jdbc:oracle:thin:@localhost:1521:xe", "mindware", "2101");
-		return c;
-	}
-
-	public static void main(String[] args) throws ClassNotFoundException,
-			SQLException {
-		UserDao dao = new UserDao();
-
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
-
-		dao.add(user);
-
-		System.out.println(user.getId() + " 등록성공");
-
-		User user2 = dao.get(user.getId());
-
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-
-		System.out.println(user2.getId() + " 조회성공");
-
-	}
+	//public abstract Connection getConnection() throws ClassNotFoundException,SQLException;
+	
+	
+	/*public class NUserDao extends UserDao {
+		public Connection getConnection()throws ClassNotFoundException,
+		SQLException {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection c = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:xe", "mindware", "2101");
+			return c;
+		}
+	}*/
+	
 }
